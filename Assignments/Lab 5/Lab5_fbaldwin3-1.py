f324eb478d5590f58f797d909173527c04b65ba9
NeonFinn <n3onfinn@gmail.com>
2025-02-07 14:09:43 -0500

    Lab 3 Projects

--- Assignments/Lab 5/Lab5_fbaldwin3-1.py
+++ Assignments/Lab 5/Lab5_fbaldwin3-1.py
@@ -9,32 +9,33 @@ while user_input.lower() != 'quit':
     roll1 = random.choice(dice)
     roll2 = random.choice(dice)
 
-    print(f'You rolled a {roll1} and a {roll2}!')
+    print('--------------------------------------------------')
+    print(f'Your total is {roll1 + roll2}. You rolled a {roll1} and a {roll2}!')
 
-    if roll1 == 1 and roll2 == 1:
-        print('Snake Eyes!')
+    if roll1 + roll2 == 2:
+        print('Snake Eyes')
     elif roll1 + roll2 == 3:
-        print('Ace Caught a Deuce!')
+        print('Ace Caught a Deuce')
     elif roll1 == 2 and roll2 == 2:
-        print('Little Joe from Kokomo!')
+        print('Little Joe from Kokomo')
     elif roll1 + roll2 == 5:
-        print('Little Phoebe!')
+        print('Little Phoebe')
     elif roll1 == 3 and roll2 == 3:
-        print('Jimmy Hicks from the Sticks!')
+        print('Jimmy Hicks from the Sticks')
     elif roll1 == 6 and roll2 == 1:
-        print('Six Ace!')
+        print('Six Ace')
     elif roll1 == 4 and roll2 == 4:
-        print('Eighter from Decatur!')
+        print('Eighter from Decatur')
     elif roll1 + roll2 == 9:
-        print('Nina from Pasadena!')
+        print('Nina from Pasadena')
     elif roll1 == 5 and roll2 == 5:
         print('Puppy Paws!')
     elif roll1 == 6 and roll2 == 5:
-        print('Six Five No Jive!')
+        print('Six Five No Jive')
     elif roll1 + roll2 == 12:
-        print('Boxcars!')
+        print('Boxcars')
     else:
-        print('No term for this roll!')
+        print('No term for this roll')
 
     user_input = input('Press enter to roll again or type "quit" to exit: ')
 
