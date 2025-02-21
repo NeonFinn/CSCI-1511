931614521e82d17e9f39c082c736204b3f78fc88
NeonFinn <n3onfinn@gmail.com>
2025-02-20 23:12:39 -0500

    -

--- Assignments/Lab 7/Lab7_fbaldwin3-1.py
+++ Assignments/Lab 7/Lab7_fbaldwin3-1.py
@@ -7,19 +7,30 @@ input_username = input("Please provide your username: ")
 if input_username in username_password:
     input_password = input("Please provide your password: ")
     if input_password == username_password[input_username]:
+        print("-----------------------------------------")
         print(f"Welcome {input_username}!")
     else:
         os._exit(0)
 else:
     os._exit(0)
 
+# checks for guest password
 if input_password == 'guest':
-    print("Security level: 5")
-elif (any(character.isupper() for character in input_password)) and (any(character.isdigit() for character in input_password)):
-    print("Security level: 1")
+    print("Security level: 5 (Bad)")
+
+# at least one uppercase and one digit (i had to look up the solution for 'any')
+elif (any(character.isupper() for character in input_password)) and \
+        (any(character.isdigit() for character in input_password)):
+    print("Security level: 1 (Really Good)")
+
+# at least one digit
 elif any(character.isdigit() for character in input_password):
-    print("Security level: 2")
+    print("Security level: 2 (Good)")
+
+# at least one uppercase
 elif any(character.isupper() for character in input_password):
-    print("Security level: 3")
-else: # all lowercase
-    print("Security level: 4")
+    print("Security level: 3 (Decent)")
+
+# all lowercase
+else:
+    print("Security level: 4 (Not Good)")
