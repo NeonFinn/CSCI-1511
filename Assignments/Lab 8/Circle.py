c14d16289c31500bdf0a93be066255483cc8fce3
NeonFinn <n3onfinn@gmail.com>
2025-02-28 01:37:15 -0500

    -

--- Assignments/Lab 8/Circle.py
+++ Assignments/Lab 8/Circle.py
@@ -1,7 +1,7 @@
 def circle_area_finder(radius):
     circle_area = 3.14159 * radius ** 2
-    print(f"The area of the circle is {circle_area:.2f}")
+    print(f"The area of the circle is {circle_area}")
 
 def circle_circumference_finder(radius):
     circle_circumference = 2 * 3.14159 * radius
-    print(f"The circumference of the circle is {circle_circumference:.2f}")
+    print(f"The circumference of the circle is {circle_circumference}")
--- Assignments/Lab 8/Rectangle.py
+++ Assignments/Lab 8/Rectangle.py
@@ -1,7 +1,7 @@
 def rectangle_area_finder(width, height):
     rectangle_area = width * height
-    print(f"The area of the rectangle is {rectangle_area:.2f}")
+    print(f"The area of the rectangle is {rectangle_area}")
 
 def rectangle_perimeter_finder(width, height):
     rectangle_perimeter = 2 * width + 2 * height
-    print(f"The perimeter of the rectangle is {rectangle_perimeter:.2f}")
+    print(f"The perimeter of the rectangle is {rectangle_perimeter}")
