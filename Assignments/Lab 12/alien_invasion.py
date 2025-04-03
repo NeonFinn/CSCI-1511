62f44a9c60954f187633ffb32c54220ac09b235b
NeonFinn <n3onfinn@gmail.com>
2025-04-03 19:10:25 -0400

    -

--- Assignments/Lab 12/alien_invasion.py
+++ Assignments/Lab 12/alien_invasion.py
@@ -1,14 +1,17 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(self.settings.resolution)
         self.clock = pygame.time.Clock()
-        self.caption = "Alien Invasion"
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
+        pygame.display.set_caption(self.settings.game_name)
 
     def run_game(self):
         while True:
@@ -16,10 +19,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blit_me()
 
             pygame.display.flip()
-            self.clock.tick(60)
+            self.clock.tick(self.settings.frame_rate)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
---
+++ Assignments/Lab 12/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    def __init__(self):
+        self.game_name = "Alien Invasion"
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.resolution = (self.screen_width, self.screen_height)
+        self.bg_color = (230, 230, 230)
+        self.frame_rate = 60
