15be6b61372f61ff8b4d89835196129413a86fce
NeonFinn <n3onfinn@gmail.com>
2025-05-01 00:04:18 -0400

    -

--- Assignments/Lab 15/Lab15_fbaldwin3-1.py
+++ Assignments/Lab 15/Lab15_fbaldwin3-1.py
@@ -12,8 +12,11 @@ for degree in range (361):
     x_values.append(x)
     y_values.append(y)
 
+plt.style.use("dark_background")
+figure, graph = plt.subplots()
 plt.plot(x_values, y_values, color = "red", linewidth = 5)
-plt.title("Circle")
-plt.xlabel("X-axis")
-plt.ylabel("Y-axis")
+graph.set_title("Circle", fontsize = 20)
+graph.set_xlabel("X-axis", fontsize = 14)
+graph.set_ylabel("Y-axis", fontsize = 14)
+
 plt.show()
