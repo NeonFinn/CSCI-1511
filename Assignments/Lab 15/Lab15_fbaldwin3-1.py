d81e92062fa4979578c67c6c049cf91db6085ca4
NeonFinn <n3onfinn@gmail.com>
2025-05-01 00:12:16 -0400

    -

--- Assignments/Lab 15/Lab15_fbaldwin3-1.py
+++ Assignments/Lab 15/Lab15_fbaldwin3-1.py
@@ -4,8 +4,8 @@ import math
 x_values = []
 y_values = []
 
-for degree in range (361):
-    radians = math.radians(degree)
+for degree in range (361): # 0 to 360 degrees
+    radians = math.radians(degree) # Convert degrees to radians
     x = math.cos(radians)
     y = math.sin(radians)
 
@@ -13,8 +13,10 @@ for degree in range (361):
     y_values.append(y)
 
 plt.style.use("dark_background")
-figure, graph = plt.subplots()
-plt.plot(x_values, y_values, color = "red", linewidth = 5)
+
+figure, graph = plt.subplots() # Create a figure and axis
+plt.plot(x_values, y_values, color = "red", linewidth = 5) # Plot the circle
+
 graph.set_title("Circle", fontsize = 20)
 graph.set_xlabel("X-axis", fontsize = 14)
 graph.set_ylabel("Y-axis", fontsize = 14)
