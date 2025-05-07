a16d2e03bceb828b7525f9bf442da624d054eef8
NeonFinn <n3onfinn@gmail.com>
2025-05-07 12:20:34 -0400

    -

--- Assignments/Lab 16/Lab16_fbaldwin3-1.py
+++ Assignments/Lab 16/Lab16_fbaldwin3-1.py
@@ -3,36 +3,38 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-FIRST_NAME = 0
-LAST_NAME = 1
-ATTENDANCE = 2
-FAVORITE_LECTURE = 3
+# constants for columns
+DATE = 0
+OHUR = 1
 
-path = Path('names.csv')
+# read the CSV file and split it into lines
+path = Path('OHUR.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
+# skip the header row and create a reader object
 reader = csv.reader(lines)
 header_row = next(reader)
 
-dates, attendance_count = [], []
-for row in reader:
-    current_date = datetime.strptime(row[FAVORITE_LECTURE], '%m/%d/%Y')
-    dates.append(current_date)
-    attendance = int(row[ATTENDANCE])
-    attendance_count.append(attendance)
+# try to read the data, print missing data if any
+dates, ohur = [], []
+for index, row in enumerate(reader):
+    try:
+        current_date = datetime.strptime(row[DATE], '%Y-%m-%d')
+        current_ohur = float(row[OHUR])
+    except ValueError:
+        print(f"Missing data for {row[DATE]}")
+    else:
+        dates.append(current_date)
+        ohur.append(current_ohur)
 
-
-plt.style.use('dark_background')
+plt.style.use('ggplot')
 fig, ax = plt.subplots()
-ax.plot(dates, attendance_count)
+ax.plot(dates, ohur)
 
-ax.set_title('Attendance Count', fontsize=24)
-ax.set_xlabel('', fontsize=16)
+ax.set_title('Ohio Unemployment Rates: 1976 - 2022', fontsize=16)
+ax.set_xlabel('Date', fontsize=16)
 fig.autofmt_xdate()
-ax.set_ylabel('Attendance', fontsize=16)
-ax.tick_params(labelsize=16)
+ax.set_ylabel('Unemployment Rate', fontsize=16)
+ax.tick_params(labelsize=12)
 
 plt.show()
-
-
-
